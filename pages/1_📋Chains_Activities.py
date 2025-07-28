df692a7594d2d2c8602fdaac8c24c32e6f31e129
Eman Raz <121406187+Emanoel91@users.noreply.github.com>
2025-07-28 16:23:10 +0330

    Update 1_📋Chains_Activities.py

--- pages/1_📋Chains_Activities.py
+++ pages/1_📋Chains_Activities.py
@@ -14,7 +14,7 @@ st.set_page_config(
 st.title("📋Chains Activities")
 
 st.info(
-    "🔔Charts initially display data for a default time range. Select a custom range to view results for your desired period."
+    "📊Charts initially display data for a default time range. Select a custom range to view results for your desired period."
 
 )
 
