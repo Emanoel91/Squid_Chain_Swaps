629bbdc872be5658ba22a7560195edb39e620ae0
Eman Raz <121406187+Emanoel91@users.noreply.github.com>
2025-07-28 16:20:16 +0330

    Update 1_📋Chains_Activities.py

--- pages/1_📋Chains_Activities.py
+++ pages/1_📋Chains_Activities.py
@@ -15,6 +15,7 @@ st.title("📋Chains Activities")
 
 st.info(
     "🔔Charts initially display data for a default time range. Select a custom range to view results for your desired period. "
+
     "⏳On-chain data retrieval may take a few moments. Please wait while the results load."
 )
 
