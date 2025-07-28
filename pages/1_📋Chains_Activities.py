e0d753b36f7da9e540dac6cdaf1894cf49d103d8
Eman Raz <121406187+Emanoel91@users.noreply.github.com>
2025-07-28 16:19:53 +0330

    Update 1_📋Chains_Activities.py

--- pages/1_📋Chains_Activities.py
+++ pages/1_📋Chains_Activities.py
@@ -13,6 +13,11 @@ st.set_page_config(
 
 st.title("📋Chains Activities")
 
+st.info(
+    "🔔Charts initially display data for a default time range. Select a custom range to view results for your desired period. "
+    "⏳On-chain data retrieval may take a few moments. Please wait while the results load."
+)
+
 # --- Snowflake Connection ---
 conn = snowflake.connector.connect(
     user=st.secrets["snowflake"]["user"],
